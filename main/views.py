3fdbc9b31d94ef93887ca8fda6f8f29ea7603dc7
hutl <ericksoares316@gmail.com>
2023-11-01 21:34:56 -0300

    feat: create disciplinasview

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import TemplateView
+from .models import *
 from accounts.models import User
 
 class IndexView(TemplateView):
-    template_name = "main/disciplinas.html"
+    template_name = "main/index.html"
     
 class MeuPerfilView(TemplateView):
     template_name = "main/meu-perfil.html"
+
+class DisciplinasView(DetailView):
+    model = Disciplina
+    template_name = "main/disciplinas.html"
