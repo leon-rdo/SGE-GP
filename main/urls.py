8caa5c125b8bf47f15805132d6ae8756680180ba
leon-rdo <leonardoabreu1212@gmail.com>
2023-11-01 21:07:09 -0300

    feat: create personal profile page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, MeuPerfilView
 
 app_name = 'main'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('meu-perfil/', MeuPerfilView.as_view(), name='meu-perfil'),
 ]
-
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from accounts.models import User
 
 class IndexView(TemplateView):
     template_name = "main/disciplinas.html"
+    
+class MeuPerfilView(TemplateView):
+    template_name = "main/meu-perfil.html"
