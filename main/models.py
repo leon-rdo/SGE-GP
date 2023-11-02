bcc3dfaf56105825682cc5da116a24d363710eea
leon-rdo <leonardoabreu1212@gmail.com>
2023-11-02 16:49:41 -0300

    feat: add subjects buttons on subjects page

--- main/models.py
+++ main/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length=50)
+    matriculados = models.ManyToManyField('accounts.User', related_name='matriculados')
--- main/views.py
+++ main/views.py
@@ -1,13 +1,26 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import *
 from accounts.models import User
 
 class IndexView(TemplateView):
     template_name = "main/index.html"
-    
+
+
 class MeuPerfilView(TemplateView):
     template_name = "main/meu-perfil.html"
 
-class DisciplinasView(DetailView):
+
+class DisciplinasView(ListView):
     model = Disciplina
     template_name = "main/disciplinas.html"
+    context_object_name = "disciplinas"
+    
+
+class DesempenhoView(DetailView):
+    model = Disciplina
+    template_name = "main/desempenho.html"
+
+
+class AtividadesView(DetailView):
+    model = Disciplina
+    template_name = "main/atividades.html"
