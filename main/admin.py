f23693bb6b7964a4d335759c48ed7ff4ea263611
leon-rdo <leonardoabreu1212@gmail.com>
2023-11-06 10:12:52 -0300

    feat: create models and customize admin site

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,57 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Subject)
-admin.site.register(Class)
-admin.site.register(Ativity)
+
+admin.site.site_header = "Sistema de Gestão Educacional"
+admin.site.site_title = "Administração"
+admin.site.index_title = "SGE-GP"
+admin.site.__doc__ = "Sistema de Gestão Educacional - Gestão de Professores"
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'class_code']
+    search_fields = ['name']
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
+    search_fields = ['__str__', 'code']
+    list_filter = ['academic_year', 'level']
+
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'class_code', 'date']
+    search_fields = ['subject__name']
+    list_filter = ['date']
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'date']
+    search_fields = ['school_test', 'subject__name', 'class_code']
+    list_filter = ['date', 'school_test']
+    
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ['student', 'test', 'grade']
+    search_fields = ['student', 'test']
+    readonly_fields = ['student', 'test']
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'delivery_date']
+    search_fields = ['title', 'subject__name']
+    readonly_fields = ['creation_date']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_time']
+    search_fields = ['title']
+    list_filter = ['date_time']
--- main/models.py
+++ main/models.py
@@ -2,114 +2,161 @@ from django.db import models
 
 
 class Subject(models.Model):
-    
-    name = models.CharField(max_length=50)
-    enrolled = models.ManyToManyField('accounts.User', related_name='matriculados')
-    
+    name = models.CharField("Nome", max_length=50)
+    syllabus = models.TextField("Ementa")
+    class_code = models.ForeignKey(
+        "Class", on_delete=models.CASCADE, verbose_name="Turma"
+    )
+    academic_probation = models.ManyToManyField(
+        "accounts.User", related_name="academic_probation", verbose_name="Dependência"
+    )
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = "Disciplina"
         verbose_name_plural = "Disciplinas"
-    
+
 
 class Class(models.Model):
+    LEVELS = (
+        ("M", "Maternal"),
+        ("J1", "Jardim 1"),
+        ("J2", "Jardim 2"),
+        ("1", "1ª Ano"),
+        ("2", "2ª Ano"),
+        ("3", "3ª Ano"),
+        ("4", "4ª Ano"),
+        ("5", "5ª Ano"),
+        ("6", "6ª Ano"),
+        ("7", "7ª Ano"),
+        ("8", "8ª Ano"),
+        ("9", "9ª Ano"),
+        ("1M", "1º Ano - Médio"),
+        ("2M", "2º Ano - Médio"),
+        ("3M", "3º Ano - Médio"),
+    )
 
-    code = models.CharField(max_length=10)
-    description = models.CharField(max_length=50)
-    enrolled = models.ManyToManyField('accounts.User', related_name='matriculados')
+    code = models.CharField("Código", max_length=10)
+    level = models.CharField("Nível", max_length=50)
+    enrolled = models.ManyToManyField(
+        "accounts.User", related_name="matriculados", verbose_name="Matriculados"
+    )
+    academic_year = models.IntegerField("Ano Letivo")
 
     class Meta:
         verbose_name = "Turma"
         verbose_name_plural = "Turmas"
 
     def __str__(self):
-        return self.description
-    
-    
+        return self.code + " - " + self.level + " - " + str(self.academic_year)
+
+
 class Classroom(models.Model):
-    
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    class_code = models.ForeignKey(Class, on_delete=models.CASCADE)
-    date = models.DateField()
-    attendance_list = models.ManyToManyField('accounts.User', related_name='students_present')
-    class_diary = models.TextField()
-    
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, verbose_name="Disciplina"
+    )
+    class_code = models.ForeignKey(
+        Class, on_delete=models.CASCADE, verbose_name="Turma"
+    )
+    date = models.DateField("Data")
+    attendance_list = models.ManyToManyField(
+        "accounts.User",
+        related_name="students_present",
+        verbose_name="lista de presença",
+    )
+    class_diary = models.TextField("Diário de Classe")
+
     class Meta:
         verbose_name = "Aula"
         verbose_name_plural = "Aulas"
-        
+
     def __str__(self):
-        return self.subject.name + " - " + self.class_code.description + " - " + str(self.date)
-    
-    
+        return self.subject.name + " - " + self.class_code + " - " + str(self.date)
+
+
 class Test(models.Model):
-    
-        TESTS = (
-            ('1', '1ª Avaliação'),
-            ('1R', 'Recuperação 1'),
-            ('2', '2ª Avaliação'),
-            ('2R', 'Recuperação 2'),
-            ('3', '3ª Avaliação'),
-            ('3R', 'Recuperação 3'),
-            ('4', '4ª Avaliação'),
-            ('4R', 'Recuperação 4'),
-            ('PF', 'Prova Final'),
-        )
-        
-        school_test = models.CharField(max_length=50, choices=TESTS)
-        subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-        class_code = models.ForeignKey(Class, on_delete=models.CASCADE)
-        date = models.DateField()
-        
-        class Meta:
-            verbose_name = "Prova"
-            verbose_name_plural = "Provas"
-            
-        def __str__(self):
-            return self.school_test + " - " + self.subject.name + " - " + self.class_code.description
-        
+    TESTS = (
+        ("1", "1ª Avaliação"),
+        ("1R", "Recuperação 1"),
+        ("2", "2ª Avaliação"),
+        ("2R", "Recuperação 2"),
+        ("3", "3ª Avaliação"),
+        ("3R", "Recuperação 3"),
+        ("4", "4ª Avaliação"),
+        ("4R", "Recuperação 4"),
+        ("PF", "Prova Final"),
+    )
+
+    school_test = models.CharField("Prova", max_length=50, choices=TESTS)
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, verbose_name="Disciplina"
+    )
+    class_code = models.ForeignKey(
+        Class, on_delete=models.CASCADE, verbose_name="Turma"
+    )
+    date = models.DateField("Data")
+
+    class Meta:
+        verbose_name = "Prova"
+        verbose_name_plural = "Provas"
+
+    def __str__(self):
+        return self.school_test + " de " + self.subject.name + " - " + self.class_code
+
 
 class Grade(models.Model):
-    
-    student = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    grade = models.FloatField()
-    
+    student = models.ForeignKey(
+        "accounts.User", on_delete=models.CASCADE, verbose_name="Aluno"
+    )
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, verbose_name="Prova")
+    grade = models.FloatField("Nota")
+
     class Meta:
         verbose_name = "Nota"
         verbose_name_plural = "Notas"
-        
+
     def __str__(self):
-        return self.student.first_name + " - " + self.test.school_test + " - " + self.test.subject.name + " - " + self.test.class_code.description
+        return (
+            self.student.first_name
+            + " - "
+            + self.test.school_test
+            + " - "
+            + self.test.subject.name
+            + " - "
+            + self.test.class_code
+        )
 
 
 class Activity(models.Model):
-    
-    title = models.CharField(max_length=50)
-    prompt = models.TextField()
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    creation_date = models.DateField(auto_now_add=True)
-    delivery_date = models.DateField()
-    
+    title = models.CharField("Título", max_length=50)
+    prompt = models.TextField("Enunciado")
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, verbose_name="Disciplina"
+    )
+    creation_date = models.DateField("Data de criação", auto_now_add=True)
+    delivery_date = models.DateField("Data de entrega")
+
     class Meta:
         verbose_name = "Atividade"
         verbose_name_plural = "Atividades"
-    
+
     def __str__(self):
         return self.title
-    
-    
+
+
 class Event(models.Model):
-    
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    date = models.DateField()
-    
+    title = models.CharField("Título", max_length=50)
+    description = models.TextField("Descrição")
+    date_time = models.DateTimeField("Data e Hora")
+    enrolled = models.ManyToManyField(
+        "accounts.User", related_name="participants", verbose_name="Participantes"
+    )
+
     class Meta:
         verbose_name = "Evento"
         verbose_name_plural = "Eventos"
-        
+
     def __str__(self):
         return self.title
