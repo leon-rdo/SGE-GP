9c5f3637bf7709af41294b6034d48da6b912642e
leon-rdo <leonardoabreu1212@gmail.com>
2023-11-21 16:57:13 -0300

    feat: create classes and events CRUD

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path("disciplinas/<int:pk>/atividades/", AtividadesView.as_view(), name="activities"),
     path("disciplinas/<int:pk>/atividades/<int:id>/", AtividadeView.as_view(), name="activity"),
     path("lancar-aula/", LancarAulaView.as_view(), name="launch-class"),
+    path("eventos/", EventosView.as_view(), name="events"),
+    path("eventos/<int:pk>/", EventoView.as_view(), name="event"),
+    path("eventos/<int:pk>/editar/", EditarEvento.as_view(), name="edit-event"),
+    path("criar-evento/", CriarEvento.as_view(), name="create-event"),
+    path("turmas/", TurmasView.as_view(), name="classes"),
+    path('turmas/<int:pk>/', TurmaDetailView.as_view(), name='class'),
+    path('turmas/<int:pk>/editar', TurmaUpdateView.as_view(), name='edit-class'),
+    path("turmas/criar/", TurmaCreateView.as_view(), name="create-class"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
 from .models import *
-from django.views.generic.edit import CreateView
 from .forms import ClassroomForm
+
 
 class IndexView(TemplateView):
     template_name = "main/index.html"
@@ -21,7 +21,6 @@ class DisciplinasView(ListView):
 class DesempenhoView(DetailView):
     model = Subject
     template_name = "main/desempenho.html"
-
 
 
 class AtividadesView(ListView):
@@ -44,3 +43,56 @@ class LancarAulaView(CreateView):
         kwargs = super(LancarAulaView, self).get_form_kwargs()
         kwargs.update({'user': self.request.user})
         return kwargs
+    
+    
+class EventosView(ListView):
+    model = Event
+    template_name = "main/eventos.html"
+    context_object_name = "events"
+    
+    
+class EventoView(DetailView):
+    model = Event
+    template_name = "main/evento.html"
+    
+    
+class CriarEvento(CreateView):
+    model = Event
+    fields = ['image', 'title', 'description', 'date_time']
+    template_name = "main/criar-evento.html"
+    success_url = reverse_lazy("main:events")
+    
+
+class EditarEvento(UpdateView):
+    model = Event
+    fields = ['image', 'title', 'description', 'date_time']
+    template_name = "main/criar-evento.html"
+    success_url = reverse_lazy("main:events")
+    
+
+class TurmasView(ListView):
+    model = Class
+    template_name = "main/turmas.html"
+    context_object_name = "classes"
+    
+
+class TurmaDetailView(DetailView):
+    model = Class
+    template_name = "main/turma.html"
+    context_object_name = "class"
+    
+    
+class TurmaUpdateView(UpdateView):
+    model = Class
+    fields = ['code', 'level', 'academic_year', 'enrolled']
+    template_name = "main/editar-turma.html"
+    success_url = reverse_lazy("main:classes")
+
+
+class TurmaCreateView(CreateView):
+    model = Class
+    fields = ['code', 'level', 'academic_year']
+    template_name = "main/criar-turma.html"
+    success_url = reverse_lazy("main:classes")
+
+
